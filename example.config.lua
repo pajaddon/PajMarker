0595cb1c060d0320f6017316291ce2e983330f7c
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-08-01 15:58:11 +0200

    Don't include config.lua in builds
    There's an example lua file here now instead

---
+++ example.config.lua
@@ -0,0 +1,16 @@
+-- Example configuration
+
+lists = {};
+
+lists["vael"] = {};
+lists["vael"]["Death Talon Wyrmkin"] = {DIAMOND, STAR}; -- Sleep targets
+lists["vael"]["Death Talon Captain"] = {TRIANGLE}; -- Kite target
+lists["vael"]["Death Talon Flamescale"] = {CROSS, SQUARE, CIRCLE}; -- DPS target
+lists["vael"]["Death Talon Seether"] = {SKULL, CROSS, SQUARE, CIRCLE}; -- DPS target
+
+lists["garr"] = {};
+lists["garr"]["Firesworn"] = {CROSS, SSQUARE, MOON, TRIANGLE, DIAMOND, CIRCLE, STAR} -- Adds
+lists["garr"]["Garr"] = {SKULL} -- Boss
+
+lists["Bat packs"] = {};
+lists["Bat packs"]["Gurubashi Bat Rider"] = {SKULL, CROSS}
